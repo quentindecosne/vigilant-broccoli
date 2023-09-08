c57a0c3604b936c03e325f82c9d1ff67426523df
Quentin <qdecosne@gmail.com>
2023-09-08 11:15:52 +0530

    add projects

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('projects.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        Project::create($request->validated());
+        // notify()->success('Project created!');
+        return redirect('/projects');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        $user = Auth::user();
+        // if ($user->can('delete projects')){
+            
+        //     // $count = Survey::where('project_id', $id)->count();
+
+        //     // if ($count > 0)
+        //     //     return redirect(route('projects'))->with('error', trans("this project has surveys"));
+
+        //     Project::destroy($project);
+        //     return redirect(route('projects'))->with('success', trans('app.record_deleted', ['field' => 'project']));
+        // }
+        // return abort(403, trans('error.unauthorized'));
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,15 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ route('projects.index') }}" :active="request()->routeIs('projects.index')">
+                        {{ __('Projects') }}
+                    </x-nav-link>
+                </div>
+
+                
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -69,6 +78,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('projects.index')" :active="request()->routeIs('projects')">
+                {{ __('Projects') }}
             </x-responsive-nav-link>
         </div>
 
