6cdf9f6f352475df5b3d2d0022f0592d8bd1e8c1
Quentin <qdecosne@gmail.com>
2023-09-12 11:50:34 +0530

    remove unneeded formrequest, change delete button

--- app/Http/Livewire/ProjectsTable.php
+++ app/Http/Livewire/ProjectsTable.php
@@ -129,7 +129,7 @@ final class ProjectsTable extends PowerGridComponent
     public function columns(): array
     {
         return [
-            Column::make('Id', 'id'),
+            Column::make('Id', 'id')->hidden(),
             Column::make('Name', 'name')
                 ->sortable()
                 ->searchable(),
@@ -191,8 +191,11 @@ final class ProjectsTable extends PowerGridComponent
     public function actions(): array
     {
        return [
-           Button::make('destroy', 'Delete')
-               ->class('bg-red-500 cursor-pointer text-white px-3 py-2 m-1 rounded text-sm')
+           Button::make('destroy', '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+           <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+         </svg>
+         ')
+               ->class('inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-red-700 bg-white hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500')
                ->openModal('projects-delete', ['project' => 'id',]),
         ];
     }
