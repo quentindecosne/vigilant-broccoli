b4810bbb8148bb64d7cdcaa681bf15cb7f38b0aa
Quentin <qdecosne@gmail.com>
2023-12-14 11:17:04 +0530

    Create master survey table

---
+++ app/Models/PlantSurveyMaster.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlantSurveyMaster extends Model
+{
+    use HasFactory;
+    protected $table = 'plant_survey_master';
+    protected $fillable = ['survey_id', 'plant_id', 'number_present', 'occurrence', 'regeneration', 'note'];
+}
