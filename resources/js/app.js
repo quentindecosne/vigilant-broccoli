bf7ccba4d08eef81080851580dd94d3b7ad84d29
Quentin <qdecosne@gmail.com>
2023-12-20 16:37:48 +0530

    add jquery to simplify ajax calls

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import './bootstrap';
 
-
+import $ from 'jquery';
 import Alpine from 'alpinejs';
 import flatpickr from "flatpickr";
 
+window.jQuery = window.$ = $;
 window.Alpine = Alpine;
 window.flatpickr = flatpickr
 
@@ -13,3 +14,6 @@ import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid.css'
 
 Alpine.start();
+
+
+
