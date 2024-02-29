21b45b8d547278590137f276af3f193f8c9d62d9
Quentin <qdecosne@gmail.com>
2024-02-29 10:32:19 +0530

    change dropdown in master survey regeneration #24

--- resources/views/components/occurrence-select.blade.php
+++ resources/views/components/occurrence-select.blade.php
@@ -3,7 +3,7 @@
     <select data-url="{{ $url }}" data-type="{{ $type }}"
             class="@if ($selected != "") border-gray-300 @else border-amber-300 @endif master-update-select max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm  rounded-md">
         @if ($selected == "" )
-            <option value="" selected="selected">Set occurrence</option>
+            <option value="" selected="selected">Set {{$type}}</option>
         @endif
         @foreach ($options as $option)
             <option
