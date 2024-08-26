ffa1a28e83868c32c9aaa058defb66ed76a813c9
quentindecosne <qdecosne@gmail.com>
2024-08-26 10:49:08 +0530

    hide list button if no list

--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -35,26 +35,28 @@
                                     days ago)
                                 </dd>
                             </div>
-                            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 hidden">
-                                <dt class="text-sm font-medium text-gray-500">Species list</dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            @if ($survey->species_list)
+                                <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 hidden">
+                                    <dt class="text-sm font-medium text-gray-500">Species list</dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
 
-                                    <div class="w-0 flex-1 flex items-center">
-                                        <svg class="flex-shrink-0 h-5 w-5 text-gray-400"
-                                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                             fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                  d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                                                  clip-rule="evenodd"/>
-                                        </svg>
-                                        <button class="mr-1 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium">
-                                            <a href="{{ route('download.species_list', ['survey_id' => $survey->id]) }}"
-                                               class="text-white bg-teal-500 p-2">Download</a>
-                                        </button>
+                                        <div class="w-0 flex-1 flex items-center">
+                                            <svg class="flex-shrink-0 h-5 w-5 text-gray-400"
+                                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                 fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd"
+                                                      d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                                                      clip-rule="evenodd"/>
+                                            </svg>
+                                            <button class="mr-1 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium">
+                                                <a href="{{ route('download.species_list', ['survey_id' => $survey->id]) }}"
+                                                   class="text-white bg-teal-500 p-2">Download</a>
+                                            </button>
 
-                                    </div>
-                                </dd>
-                            </div>
+                                        </div>
+                                    </dd>
+                                </div>
+                            @endif
                         </dl>
                     </div>
                     <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 hidden">
